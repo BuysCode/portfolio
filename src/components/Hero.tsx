f501e554a52accfe46e4539e57d4b112b155c080
Guilherme Buys <guibuys55@gmail.com>
2026-01-19 01:20:32 -0300

    feat: some changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import { Download } from 'lucide-react'
 export default function Hero() {
     return (
         <div id="#inicio" className="w-full flex md:flex-row justify-around items-center mt-20 flex-col-reverse">
-            <div className="flex flex-col space-y-2 text-center md:text-justify">
+            <div className="flex flex-col space-y-4 text-center md:text-justify mt-8">
                 <div className="flex flex-row space-x-4">
                     <Badge text="HTML" color="red"/>
                     <Badge text="CSS" color="blue" />
@@ -18,11 +18,11 @@ export default function Hero() {
                 </div>
                 <h1 className="font-bold text-3xl">Olá, meu nome é <span className="text-blue-500">Guilherme Buys</span></h1>
                 <p>Sou um desenvolvedor FullStack em busca do primeiro emprego na área</p>
-                <div className="flex flex-row space-x-4">
+                <div className="flex flex-row space-x-4 justify-center md:justify-start">
                     <Link href="/docs/CV.pdf" className="bg-blue-600 hover:bg-blue-700 rounded-lg text-lg items-center flex flex-row gap-4 p-4" download={'/docs/CV.pdf'}><Download></Download> Meu Currículo</Link>
                 </div>
             </div>
-            <img className="rounded-full border-4 border-blue-500" src={"https://avatars.githubusercontent.com/u/131329633?v=4"} alt="Foto de perfil Buys Code"/>
+            <img className="rounded-full border-4 border-blue-500 h-80 w-80 md:w-100 md:h-100" src={"https://avatars.githubusercontent.com/u/131329633?v=4"} alt="Foto de perfil Buys Code"/>
         </div>
     )
 }
--- src/components/ProjectsPreview.tsx
+++ src/components/ProjectsPreview.tsx
@@ -3,10 +3,10 @@ import { BiRightArrowAlt } from "react-icons/bi";
 
 export default function ProjectsPreview() {
     return (
-        <div className="mt-20 px-8 w-full">
+        <div className="mt-20 px-8">
             <h2 className="text-2xl font-bold text-center">Projetos</h2>
             <div className="flex flex-col mt-10">
-                <Link href={'/projects'} className="flex flex-row space-x-4 hover:underline hover:text-blue-600">Ver todos meus projetos <BiRightArrowAlt size={24} /></Link>
+                <Link href={'/projects'} className="flex text-xl flex-row space-x-4 hover:underline hover:text-blue-600 items-center justify-end">Ver todos meus projetos <BiRightArrowAlt size={24} /></Link>
                 <div className="grid grid-cols-1 md:grid-cols-3">
                 </div>
             </div>
