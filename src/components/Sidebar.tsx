c230a448ab48fb0b06dd653f6f78b31e479b521a
Guilherme Buys <guibuys55@gmail.com>
2026-01-21 14:24:46 -0300

    feat: sidebar right border

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -14,7 +14,7 @@ function SidebarLink({ href, toggleSidebar, text }: Omit<SidebarProps, 'isOpen'>
 export default function Sidebar({ isOpen, toggleSidebar }: SidebarProps) {
     return (
             isOpen && (
-            <div className={`fixed top-0 left-0 h-full w-64 bg-gray-950 text-white transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} z-50 md:hidden block transition-transform duration-300 ease-in-out`}>
+            <div className={`fixed top-0 left-0 h-full w-64 bg-gray-950 text-white transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} z-50 md:hidden block transition-transform duration-300 ease-in-out border-r border-gray-700`}>
                 <div className="p-4">
                     <h2 className="text-2xl font-bold mt-2 ml-4">BuysCode <span className="text-blue-500">{"</>"}</span></h2>
                     <ul className="mt-12">
